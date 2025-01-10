08adda28092fb7f4a02361237cb8cab8812f3151
Mohamed Khedr <mohamed.khedr@afaqy.com>
2025-01-10 20:42:29 +0200

    Add auth defaults

--- config/core.php
+++ config/core.php
@@ -4,7 +4,7 @@ return [
 
     'auth' => [
         'url' => env('AUTH_SERVICE_URL'),
-        'validate_token_url' => env('AUTH_SERVICE_URL').'/api/v1/auth/validate',
+        'validate_token_url' => env('AUTH_SERVICE_VALIDATE_TOKEN_URL'),
         'defaults' => [
             'model' => env('AUTH_USER_MODEL', App\Models\User::class),
             'guard' => env('AUTH_GUARD', 'api'),
